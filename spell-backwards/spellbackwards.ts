e312059f2b256ea98e0f903eda9d79604fa1f2ad
Jim Nelson <jim@jimnelson.us>
2021-09-01 23:16:32 -0600

    switched to querySelector; allow >1 copies on page

--- spell-backwards/spellbackwards.ts
+++ spell-backwards/spellbackwards.ts
@@ -6,8 +6,8 @@
 	class SpellBackwards extends HTMLElement {
 		connectedCallback() {
 			document.addEventListener('DOMContentLoaded', () => {
-				this.input = document.getElementById("sb-input") as HTMLTextAreaElement;
-				this.output = document.getElementById("sb-output") as HTMLTextAreaElement;
+				this.input = this.querySelector('textarea:nth-child(1)') as HTMLTextAreaElement;
+				this.output = this.querySelector('textarea:nth-child(2)') as HTMLTextAreaElement;
 				this.input.addEventListener('input', () => { this.render(); });
 				this.render();
 			});
