31539b2acf52ae056bb71f30b94ee3369537b8a2
Jim Nelson <jim@jimnelson.us>
2021-09-02 10:03:49 -0600

    this is compiling, not sure if working yet

--- spell-backwards/custom-gtag.ts
+++ spell-backwards/custom-gtag.ts
@@ -1,13 +1,16 @@
+// import * as gtag from "gtag.js";
+import "gtag.js";
+
 // Measure click engagement with app in global site tag analytics
 // Pass it the custom tag name, and it will send click events on those elements
 //   includes the first class name in the event details
-export function measureClicks(customTagName) {
-    document.getElementsByTagName(customTagName)
-        .array.forEach(e => {
+export function measureClicks(customTagName: string) {
+    Array.from(document.getElementsByTagName(customTagName))
+        .forEach(e => {
             e.addEventListener('click',
                 (ee) => {
                     if (typeof gtag != 'undefined') {
-                        gtag('event', customTagName, { 'event_category': 'engagement', 'event_label': ee.target.tagName + '.' + ee.target.classList.item(0) } )
+                        gtag('event', customTagName, { 'event_category': 'engagement', 'event_label': (ee.target as Element).tagName + '.' + (ee.target as Element).classList.item(0) } )
                     }
                 }
             );
@@ -16,12 +19,12 @@ export function measureClicks(customTagName) {
 
 // Count input box changes estimating characters typed
 // Grabs *all* instances of that input tag type
-    export function measureInputs(appName, inputTagName = 'textarea') {
+    export function measureInputs(appName: string, inputTagName: string = 'textarea') {
     let inputChanges = 0;
-    let inputTimerId = null;
-    const inputChangeDelaySecs = 3;
-    document.getElementsByTagName(inputTagName)
-        .array.forEach(e => {
+    let inputTimerId:number = null;
+    const inputChangeDelaySecs: number = 3;
+    Array.from(document.getElementsByTagName(inputTagName))
+        .forEach(e => {
             e.addEventListener('input', () => {
                     inputChanges++;
                     if (inputTimerId == null) {
@@ -30,7 +33,7 @@ export function measureClicks(customTagName) {
                 }
             );
         });
-    let pushChangeCount = () => {
+    let pushChangeCount = () : void => {
         console.log(inputChanges);
         if (typeof gtag != 'undefined') {
             gtag('event', `${appName}-inputs`, { 'event_category': 'engagement', 'event_label': 'input-count', value: inputChanges } )
