25c8dc8f7ba2c229cdcc83e597b65df78284c8ed
Jim Nelson <jim@jimnelson.us>
2021-09-02 11:54:54 -0600

    Got gtag declared properly

--- spell-backwards/custom-gtag.ts
+++ spell-backwards/custom-gtag.ts
@@ -1,5 +1,4 @@
-// Function is better than any, but still want to figure out how to tell it it's @types/gtag.js
-declare const gtag: Function;
+declare const gtag: Gtag.Gtag;
 
 // Measure click engagement with app in global site tag analytics
 // Pass it the custom tag name, and it will send click events on those elements
