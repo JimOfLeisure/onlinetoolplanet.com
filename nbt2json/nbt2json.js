6a38a9666ffe01703ba083c6ddbda00ee1e054db
Jim Nelson <jim@jimnelson.us>
2021-09-26 12:46:22 -0600

    yet another change from 20 days ago

--- nbt2json/nbt2json.js
+++ nbt2json/nbt2json.js
@@ -3,10 +3,21 @@
     class ConvertNbt extends HTMLElement {
         connectedCallback() {
             console.log('connected callback');
+            document.addEventListener('DOMContentLoaded', () => {
+                this.nbt2jsonButton = document.getElementById('otp-nbt2json');
+                this.json2nbtButton = document.getElementById('otp-json2nbt');
+                this.jsonTextArea = document.getElementById('otp-json-text');
+                this.json2nbtButton.addEventListener('click', () => { console.log('click');});
+                this.nbt2jsonButton.addEventListener('click', () => { console.log('click');});
+                this.jsonTextArea.value = "Hi";
+            })
         }
         render() {
             console.log("render");
         }
     }
+    nbt2jsonButton: HTMLButtonElement;
+    json2nbtButton: HTMLButtonElement;
+    jsonTextArea: HTMLTextAreaElement;
     window.customElements.define('convert-nbt', ConvertNbt);
 })()
