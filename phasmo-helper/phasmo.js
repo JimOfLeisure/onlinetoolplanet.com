44e1c64618138e5b6854e26022f17c91da5cab62
Jim Nelson <jim@jimnelson.us>
2022-02-05 14:44:34 -0800

    refactored phasmo-helper to use webcomponents

--- phasmo-helper/phasmo.js
+++ phasmo-helper/phasmo.js
@@ -114,93 +114,97 @@
         },
     ]
 
-    let app;
-    let evidenceDiv;
-    let ghostsDiv;
+    class PhasmoHelpoer extends HTMLElement {
+        evidenceDiv;
+        ghostsDiv;
+            connectedCallback() {
+            console.log('phasmo-helper connectedCallback');
+            // TODO: apparently failing to find query result does not trigger 'or' alternative
+            this.evidenceDiv = document.querySelector('div:nth-of-type(1)') || document.createElement('div');
+            this.ghostsDiv = document.querySelector('div:nth-of-type(2)') || document.createElement('div');
 
-    function phasmoInit() {
-        evidenceDiv = document.querySelector('div#phasmo > div:nth-of-type(1)') || document.createElement('div');
-        ghostsDiv = document.querySelector('div#phasmo > div:nth-of-type(2)') || document.createElement('div');
-        ghostTypes.forEach((e, i) => {
-            e.possible = true;
-            e.div = ghostsDiv.querySelector(`div:nth-of-type(${i+1})`)
-            if (e.div === null) {
-                e.div = document.createElement('div');
-                const myLabel = document.createTextNode(e.name);
-                e.div.appendChild(myLabel)
-                ghostsDiv.appendChild(e.div);
+            // FIXME: this code is from before WebComponent refactor, need to ensure code can handle
+            //  whether or not interior elements are already defined.
+            // if (!this.evidenceDiv.isConnnected) { app.appendChild(this.evidenceDiv) };
+            // if (!this.ghostsDiv.isConnnected) { app.appendChild(this.ghostsDiv) };
+
+            ghostTypes.forEach((e, i) => {
+                e.possible = true;
+                e.div = this.ghostsDiv.querySelector(`div:nth-of-type(${i+1})`)
+                if (e.div === null) {
+                    e.div = document.createElement('div');
+                    const myLabel = document.createTextNode(e.name);
+                    e.div.appendChild(myLabel)
+                    this.ghostsDiv.appendChild(e.div);
+                }
+            })
+    
+            const evidenceList = this.evidenceDiv.querySelector('ul') || document.createElement('ul');
+            if (!evidenceList.isConnected) { this.evidenceDiv.appendChild(evidenceList); };
+    
+            evidence.forEach((e, i) => {
+                const idName = e.name.toLowerCase().replace(/ /, '-');
+                e.possible = true;
+                e.item = evidenceList.querySelector(`li:nth-of-type(${i+1})`) || document.createElement('li');
+                e.checkbox = e.item.querySelector('input') || document.createElement('input');
+                e.checkbox.type = 'checkbox';
+                e.checkbox.id = idName;
+                if (!e.checkbox.isConnected) { e.item.appendChild(e.checkbox); };
+                const myLabel = e.item.querySelector('label') || document.createElement('label');
+                myLabel.innerText = e.name;
+                myLabel.htmlFor = idName;
+                if (!myLabel.isConnected) { e.item.appendChild(myLabel); };
+                e.item.addEventListener('click', () => { e.checkbox.cl})
+                if (!e.item.isConnected) { evidenceList.appendChild(e.item); };
+            })
+    
+            this.evidenceDiv.addEventListener('click', this.phasmoUpdate);    
+        }
+
+        phasmoUpdate(e) {
+            const checkCount = evidence.reduce((a, e) => e.checkbox.checked ? a+1 : a, 0);
+            ghostTypes.forEach(e => {
+                e.div.classList.remove(errorClass)
+            });
+            evidence.forEach(e => {
+                e.item.classList.remove(errorClass)
+            });    if (checkCount == 0) {
+                // clear all formats
+                ghostTypes.forEach(e => e.div.classList.remove(impossibleClass));
+            } else if (checkCount > maxEvidence) {
+                console.error('input error')
+                ghostTypes.forEach(e => {
+                    e.div.classList.remove(impossibleClass)
+                    e.div.classList.add(errorClass)
+                });
+                evidence.forEach(e => {
+                    e.item.classList.remove(impossibleClass)
+                    e.item.classList.add(errorClass)
+                });
+            } else {
+                // process
+                evidence.forEach(e => e.possible = false);
+                ghostTypes.forEach(e => {
+                    e.possible = e.evidence.reduce((a, e) => evidence[e].checkbox.checked ? a+1 : a, 0) == checkCount;
+                    if (e.possible) {
+                        e.div.classList.remove(impossibleClass);
+                        e.evidence.forEach(e => evidence[e].possible = true);
+                    } else {
+                        e.div.classList.add(impossibleClass);
+                    }
+                });
+                evidence.forEach(e => {
+                    if (e.possible) {
+                        e.item.classList.remove(impossibleClass);
+                    } else {
+                        e.item.classList.add(impossibleClass);
+                    }
+                });
             }
-        })
-
-        const evidenceList = evidenceDiv.querySelector('ul') || document.createElement('ul');
-        if (!evidenceList.isConnected) { evidenceDiv.appendChild(evidenceList); };
-
-        evidence.forEach((e, i) => {
-            const idName = e.name.toLowerCase().replace(/ /, '-');
-            e.possible = true;
-            e.item = evidenceList.querySelector(`li:nth-of-type(${i+1})`) || document.createElement('li');
-            e.checkbox = e.item.querySelector('input') || document.createElement('input');
-            e.checkbox.type = 'checkbox';
-            e.checkbox.id = idName;
-            if (!e.checkbox.isConnected) { e.item.appendChild(e.checkbox); };
-            const myLabel = e.item.querySelector('label') || document.createElement('label');
-            myLabel.innerText = e.name;
-            myLabel.htmlFor = idName;
-            if (!myLabel.isConnected) { e.item.appendChild(myLabel); };
-            e.item.addEventListener('click', () => { e.checkbox.cl})
-            if (!e.item.isConnected) { evidenceList.appendChild(e.item); };
-        })
-
-        evidenceDiv.addEventListener('click', phasmoUpdate);
-
-    }
-
-    function phasmoUpdate(e) {
-        const checkCount = evidence.reduce((a, e) => e.checkbox.checked ? a+1 : a, 0);
-        ghostTypes.forEach(e => {
-            e.div.classList.remove(errorClass)
-        });
-        evidence.forEach(e => {
-            e.item.classList.remove(errorClass)
-        });    if (checkCount == 0) {
-            // clear all formats
-            ghostTypes.forEach(e => e.div.classList.remove(impossibleClass));
-        } else if (checkCount > maxEvidence) {
-            console.error('input error')
-            ghostTypes.forEach(e => {
-                e.div.classList.remove(impossibleClass)
-                e.div.classList.add(errorClass)
-            });
-            evidence.forEach(e => {
-                e.item.classList.remove(impossibleClass)
-                e.item.classList.add(errorClass)
-            });
-        } else {
-            // process
-            evidence.forEach(e => e.possible = false);
-            ghostTypes.forEach(e => {
-                e.possible = e.evidence.reduce((a, e) => evidence[e].checkbox.checked ? a+1 : a, 0) == checkCount;
-                if (e.possible) {
-                    e.div.classList.remove(impossibleClass);
-                    e.evidence.forEach(e => evidence[e].possible = true);
-                } else {
-                    e.div.classList.add(impossibleClass);
-                }
-            });
-            evidence.forEach(e => {
-                if (e.possible) {
-                    e.item.classList.remove(impossibleClass);
-                } else {
-                    e.item.classList.add(impossibleClass);
-                }
-            });
         }
     }
 
     document.addEventListener("DOMContentLoaded", function() {
-        app = document.getElementById('phasmo');
-        phasmoInit();
-        if (!evidenceDiv.isConnnected) { app.appendChild(evidenceDiv) };
-        if (!ghostsDiv.isConnnected) { app.appendChild(ghostsDiv) };
+        window.customElements.define('phasmo-helper', PhasmoHelpoer)
     });
 })();
